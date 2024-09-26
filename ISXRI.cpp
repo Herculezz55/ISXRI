f9a83a55263e4eaf94bf5d0178aea66cd968c65a
Adam Bennett <adam-bennett@live.com>
2024-09-26 15:07:38 -0700

    version

--- ISXRI.cpp
+++ ISXRI.cpp
@@ -11,8 +11,8 @@
 // is newer than the compared version.  With that said, use whatever version numbering system you'd like.
 
 // need to delete old file before trying to rename.
-#define EXTENSION_VERSION "6.92 9-26-24"
-double EXTVER = 6.92;
+#define EXTENSION_VERSION "6.93 9-26-24"
+double EXTVER = 6.93;
 #include "ISXRI.h"
 
 //
